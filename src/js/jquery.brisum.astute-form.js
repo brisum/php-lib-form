c91d7f85f64759035828baa032d790b5ee77b4d4
Oleksandr Manchenko <omanchenko@brisum.com>
2017-11-15 11:18:07 +0200

    Made module for js code

--- src/js/jquery.brisum.astute-form.js
+++ src/js/jquery.brisum.astute-form.js
@@ -1,79 +1,180 @@
-;(function (window, $, UNDEFINED) {
-    window.Brisum = window.Brisum || {};
+(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(function($) {
+    ;(function ($) {
+        var AstuteForm = function ($form, options) {
+            this.$form = $form;
+            this.settings = $.extend(
+                this.defaultSettings,
+                {
+                    type: this.$form.attr('method'),
+                    url: this.$form.attr('data-url'),
+                    beforeSend: this.$form.attr('data-before-send'),
+                    afterSend: this.$form.attr('data-after-send'),
+                    success: this.$form.attr('data-success'),
+                    error: this.$form.attr('data-error'),
+                    messageError: this.$form.attr('data-message-error')
+                },
+                options || {}
+            );
 
-    window.Brisum.AstuteForm = {
-        init: function (name, cbStart, cbFinish) {
-            if (0 == $('#' + name).length) {
-                return true;
-            }
+            this.init();
+        };
 
-            var $form = $('#' + name);
+        AstuteForm.prototype.defaultSettings = {
+            type: 'post',
+            url: '',
+            beforeSend: undefined,
+            afterSend: undefined,
+            success: undefined,
+            error: undefined,
+            messageError: 'Извините, возникла ошибка во время отправки данных'
+        };
 
-            $form.on('click', 'input[type=submit]', function () {
-                $("input[type=submit]", $form).removeAttr("clicked");
+        AstuteForm.prototype.init = function () {
+            var self = this;
+
+            self.$form.on('click.astute-form', 'input[type=submit]', function () {
+                self.$form.find("input[type=submit]").removeAttr("clicked");
                 $(this).attr("clicked", true);
             });
 
-            $form.submit(function (event) {
-                var data = $form.serializeJSON(),
-                    $submit = $("input[type=submit][clicked]", $form),
-                    resultBefore = true;
+            self.$form.submit(function (event) {
+                var data = self.$form.serializeJSON(),
+                    $submit = self.$form.find("input[type=submit][clicked]");
 
                 if ($submit.length) {
                     data[$submit.attr('name')] = $submit.val();
                 }
 
-                if (typeof cbStart === 'function') {
-                    resultBefore = cbStart();
-                } else if (window[cbStart]) {
-                    resultBefore = window[cbStart]();
-                }
-                if (false === resultBefore) {
+                if (!self.beforeSend(data)) {
                     return false;
                 }
 
-                //Post Form with data
+                self.loading(true);
                 $.ajax({
-                    type: 'post',
-                    url: '/component-manager/request/form/listener.php',
+                    type: self.settings.type,
+                    url: self.settings.url,
                     data: data,
                     success: function (response, textStatus) {
                         var $div = $('<div></div>').html(response);
 
-                        $('#' + name).parent().html($div.html());
-
-                        Brisum.AstuteForm.init(name, cbStart, cbFinish);
-
-                        if (typeof cbFinish === 'function') {
-                            cbFinish()
-                        } else if ( window[cbFinish] ) {
-                            window[cbFinish](response);
-                        }
+                        self.$form.html($div.find('form[data-astute-form]').html());
 
                         setTimeout(function() {
-                            $('#' + name + ' .form-msg.temporary').fadeOut(1500);
+                            self.$form.find('.message.temporary').fadeOut(1500);
                         }, 2000);
                         setTimeout(function() {
-                            $('#' + name + ' .form-msg.temporary').remove();
+                            self.$form.find('.message.temporary').remove();
                         }, 4000);
+
+                        self.afterSend(response);
+                        self.success(response);
+                        self.loading(false);
                     },
                     error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        Brisum.AstuteForm.message(name, 'Извините, возникла ошибка во время отправки данных', 'alert');
+                        self.messageClear();
+                        self.message(self.settings.messageError, 'alert');
 
-                        if ( window[cbFinish] ) {
-                            window[cbFinish]('error_response');
-                        }
+                        self.afterSend(XMLHttpRequest, textStatus, errorThrown);
+                        self.error(XMLHttpRequest, textStatus, errorThrown);
+                        self.loading(false);
                     }
                 });
 
                 return false;
-            });
-        },
+            })
+        };
 
-        message: function(name, message, type)
+        AstuteForm.prototype.loading = function(isLoading)
         {
-            var messageId = 'msg-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-            $('#' + name).prepend('<div class="row"><div class="small-12 columns"><div id="' + messageId + '" class="callout ' + type + '">' + message + '</div></div></div>');
-        }
-    };
-})(window, jQuery);
+            if (isLoading) {
+                this.$form.addClass('loading');
+            } else {
+                this.$form.removeClass('loading');
+            }
+        };
+
+        AstuteForm.prototype.beforeSend = function(data)
+        {
+            var self = this;
+
+            if (typeof self.settings.beforeSend === 'function') {
+                return self.settings.beforeSend(data);
+            }
+            if (window[self.settings.beforeSend]) {
+                return window[self.settings.beforeSend](data);
+            }
+            return true;
+        };
+
+        AstuteForm.prototype.afterSend = function(response)
+        {
+            var self = this;
+
+            if (typeof self.settings.afterSend === 'function') {
+                return self.settings.afterSend(response);
+            }
+            if (window[self.settings.afterSend]) {
+                return window[self.settings.afterSend](response);
+            }
+        };
+
+        AstuteForm.prototype.success = function()
+        {
+            var self = this;
+
+            if (typeof self.settings.success === 'function') {
+                self.settings.success();
+            }
+            if (window[self.settings.success]) {
+                window[self.settings.success]();
+            }
+        };
+
+        AstuteForm.prototype.error = function(XMLHttpRequest, textStatus, errorThrown)
+        {
+            var self = this;
+
+            if (typeof self.settings.error === 'function') {
+                return self.settings.error(XMLHttpRequest, textStatus, errorThrown);
+            }
+            if (window[self.settings.error]) {
+                return window[self.settings.error](XMLHttpRequest, textStatus, errorThrown);
+            }
+        };
+
+        AstuteForm.prototype.message = function(message, type)
+        {
+            var self = this,
+                messageId = 'msg-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+            self.$form.prepend('<div id="' + messageId + '" class="astute-form-alert ' + type + '">' + message + '</div>');
+        };
+
+        AstuteForm.prototype.messageClear= function()
+        {
+            var self = this;
+            self.$form.find('.message').remove();
+        };
+
+        window.Brisum = window.Brisum || {};
+        window.Brisum.AstuteForm = AstuteForm;
+
+        $.fn.astuteForm = function(options) {
+            return this.each(function() {
+                new AstuteForm($(this), options);
+            });
+        };
+
+        $(document).ready(function () {
+            $('form[data-astute-form]').astuteForm();
+        });
+    }( jQuery ));
+}));
--- src/js/jquery.serializeJSON.js
+++ src/js/jquery.serializeJSON.js
@@ -1,31 +1,42 @@
-;(function(global, $, undefined){
-  // serializeJSON fixed
-  $.fn.serializeJSON = function(){
-    var json = {};
-    var form = $(this);
-    form.find('input, select, textarea').each(function(){
-      var val;
-      if (!this.name) return;
- 
-      if ('radio' === this.type) {
-        if (json[this.name]) { return; }
- 
-        json[this.name] = this.checked ? this.value : '';
-      } else if ('checkbox' === this.type) {
-        val = json[this.name];
- 
-        if (!this.checked) {
-          // if (!val) { json[this.name] = ''; }
-        } else {
-          json[this.name] = 
-            typeof val === 'string' ? [val, this.value] :
-            $.isArray(val) ? $.merge(val, [this.value]) :
-            this.value;
-        }
-      } else {
-        json[this.name] = this.value;
-      }
-    });
-    return json;
-  }
-})(window, jQuery);
+(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+}(function($) {
+    'use strict';
+
+    // serializeJSON fixed
+    $.fn.serializeJSON = function(){
+        var json = {};
+        var form = $(this);
+        form.find('input, select, textarea').each(function(){
+            var val;
+            if (!this.name) return;
+
+            if ('radio' === this.type) {
+                if (json[this.name]) { return; }
+
+                json[this.name] = this.checked ? this.value : '';
+            } else if ('checkbox' === this.type) {
+                val = json[this.name];
+
+                if (!this.checked) {
+                    // if (!val) { json[this.name] = ''; }
+                } else {
+                    json[this.name] =
+                        typeof val === 'string' ? [val, this.value] :
+                            $.isArray(val) ? $.merge(val, [this.value]) :
+                                this.value;
+                }
+            } else {
+                json[this.name] = this.value;
+            }
+        });
+        return json;
+    }
+}));
